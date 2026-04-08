5dede37f89b41a92ee2da8212159470caebf2da3
jihoo <jihoop0620@gmail.com>
2026-04-08 17:23:34 +0900

    .

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# DeepLearningMath" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:jihoo12/DeepLearningMath.git
-git push -u origin main
